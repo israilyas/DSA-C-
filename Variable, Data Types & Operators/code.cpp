02da6a99021b0d7316cdd1fd968e1e999d4c929a
ISRA ILYAS <israqureshi138@gmail.com>
2024-10-05 15:35:38 +0500

    studied Variables, DataTypes and operators

---
+++ Variable, Data Types & Operators/code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello World";
+    cout << "Hello World" <<endl;
+    cout << "Hello World \nFrom ISRA ILYAS\n";
+    cout << "ISRA\nILYAS";
+
+    return 0;
+}
+
+ // ; statement terminator
+
+//  Execution starts from main function
+// #include <iostream> ==> Preprocessor Direcctive
+//It is telling that There is a iostreem file in which there is logic fron "cout"
+// main function return an integer value
+
+// === Run Code ====
+
+// 1) Compile
+            // g++ code.cpp
+            // .exe  for widow user => executable file
+            // .out file for mac
+
+// 2) Run Executable File 
+// ./a.exe
+
+//  === Combine both steps ====
+
+// g++ code.cpp && ./a.exe
